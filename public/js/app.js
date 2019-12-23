673deedf572ade8994a8e76d50cf5f6abdb2fc31
zhangdongjie1109 <942802856@qq.com>
2019-12-23 17:54:06 +0800

    删除npm run watch 生产的文件

--- public/js/app.js
+++ public/js/app.js
@@ -36985,8 +36985,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\phpStudy\PHPTutorial\WWW\larabbs_v6x\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\phpStudy\PHPTutorial\WWW\larabbs_v6x\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\laragon\www\larabbs\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\laragon\www\larabbs\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
