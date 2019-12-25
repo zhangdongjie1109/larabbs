6f057b3ff79e008f942f7696d2aba5d2bce550c5
zhangdongjie1109 <942802856@qq.com>
2019-12-25 12:50:19 +0800

    生成话题骨架

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Topic;
+use App\Observers\TopicObserver;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,7 +17,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-
     }
 
     /**
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(190);
+        Topic::observe(TopicObserver::class);
     }
 }
