b288bb886d16295330c74d4fc4faf6943f001a74
zhangdongjie1109 <942802856@qq.com>
2019-12-29 21:18:50 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,4 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
