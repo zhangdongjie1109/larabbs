ee8a73dba22df83b7eb0dca83396066f0a47bfa6
zhangdongjie1109 <942802856@qq.com>
2020-01-06 17:28:26 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Reply;
 use App\Notifications\TopicReplied;
+use Illuminate\Support\Facades\DB;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -19,10 +20,14 @@ class ReplyObserver
     {
 //        $reply->topic->increment('reply_count',1);
 
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $reply->topic->updateReplyCount();
 
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
     }
 }
