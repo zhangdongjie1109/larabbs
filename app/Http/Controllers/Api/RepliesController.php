703106d5999460ee4578074f0af05deb63a64830
zhangdongjie1109 <942802856@qq.com>
2020-09-08 15:42:58 +0800

    话题回复

---
+++ app/Http/Controllers/Api/RepliesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\ReplyRequest;
+use App\Http\Resources\ReplyResource;
+use App\Models\Reply;
+use App\Models\Topic;
+
+class RepliesController extends Controller
+{
+    public function store(ReplyRequest $request, Topic $topic, Reply $reply)
+    {
+        $reply->content = $request->content;
+        $reply->topic()->associate($topic);
+        $reply->user()->associate($request->user());
+        $reply->save();
+
+        return new ReplyResource($reply);
+    }
+}
